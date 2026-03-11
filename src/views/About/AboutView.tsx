4a917523b3db16594c188bee8e870306a4257656
Apoorv Tiwari <apurv9950@gmail.com>
2026-03-11 13:53:00 +0530

    Update AboutView.tsx (#2)
    * Update AboutView.tsx
    
    * Update WelcomeView.tsx

--- src/views/About/AboutView.tsx
+++ src/views/About/AboutView.tsx
@@ -1,5 +1,3 @@
-import { Card, CardActions, CardContent, CardHeader, Divider, Grid, Typography } from '@mui/material';
-import { AppButton, AppLink, AppIconButton, AppView } from '../../components';
 import DialogsSection from './DialogsSection';
 
 /**
